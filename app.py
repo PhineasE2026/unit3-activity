0c6c9ab29e4028dc03e215c802861030f1411858
finny <phineas.ehrlich2026@gbwl.org>
2026-03-06 14:39:54 +0000

    finished quiz

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+from collections import Counter
+import random
 
 app = Flask(__name__)
 
@@ -18,32 +20,62 @@ def submit():
     politicians = [
         {
             "name": "Benjamin Netanyahu",
-            "image": "images/netanyahu.jpg",
-            "description": "You are spiritually Jewish"
+            "image": "netanyahu.jpg",
+            "description": "You are evil..."
         },
         {
             "name": "Donald Trump",
-            "image": "images/trump.jpg",
-            "description": "You are"
+            "image": "trump.jpg",
+            "description": "You are very Israeli"
         },
         {
             "name": "Barack Obama",
-            "image": "images/obama.webp",
-            "description": "Measured, polished, and message-focused."
+            "image": "obama.webp",
+            "description": "You are fairly Israeli, but you are Aert"
         },
         {
             "name": "Zohran Mamdani",
-            "image": "images/mamdani.jpg",
-            "description": "Ideological, activist, and insurgent-style."
+            "image": "mamdani.jpg",
+            "description": "The most Israeli politicianm, you are the most Israeli!"
         },
         {
             "name": "Xi Jinping",
-            "image": "images/xi_jinping.jpg",
-            "description": "Centralized, disciplined, and control-oriented."
+            "image": "xi_jinping.jpg",
+            "description": "you are not israel because you are China"
         },
     ]
+
+    politician = {
+        "tomato": "Donald Trump",
+        "blueberry": "Xi Jinping",
+        "olive": "Benjamin Netanyahu",
+        "date": "Barack Obama",
+
+        "germany": "Zohran Mamdani",
+        "russia": "Donald Trump",
+        "usa": "Benjamin Netanyahu",
+        "brazil": "Xi Jinping",
+
+        "football": "Benjamin Netanyahu",
+        "american_football": "Barack Obama",
+        "basketball": "Xi Jinping",
+        "ice_hockey": "Zohran Mamdani",
+    }
+
+    options = [
+        politician.get(user_input['q1']),
+        politician.get(user_input['q2']),
+        politician.get(user_input['q3'])
+    ]
+    options = [option for option in options if option]
     
-    return render_template('result.html')
+    count = Counter(options)
+    max_score = max(count.values())
+    tied = [name for name, score in count.items() if score == max_score]
+    winner_name = random.choice(tied)
+
+    result = next(option for option in politicians if option['name'] == winner_name)
+    return render_template('result.html', result=result)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
